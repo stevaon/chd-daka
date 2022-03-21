7bd1b7c901762d56cbb02bfd279db8d7ce3ca0c0
lttlerZer <2401997075@qq.com>
2022-03-21 11:36:35 +0800

    fix

---
+++ main-action.py
@@ -0,0 +1,124 @@
+import time
+import os
+import json
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+def task(username, password, address, position, wxkey):
+    chrome_option = Options()
+
+    chrome_option.add_argument('--headless')
+    chrome_option.add_argument('--no-sandbox')
+    chrome_option.add_argument('window-size=1920x1080') # 指定浏览器分辨率
+    chrome_option.add_argument('--disable-gpu')
+    chrome_option.add_experimental_option('excludeSwitches', ['enable-automation'])
+    # action端
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_option)
+    # driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_option)
+    # win端
+    # driver = webdriver.Chrome(options=chrome_option)
+    # driver = webdriver.Chrome()
+    # driver.set_window_size(500, 940)
+    #登录
+    output_data = ""
+    url_login='https://cdjk.chd.edu.cn'
+    driver.get(url_login)
+    time.sleep(2)
+    driver.find_element_by_xpath('//*[@id="username"]').send_keys(username)
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="password"]').send_keys(password,Keys.ENTER)
+
+    # 判断是否在打卡时间段
+    try:
+        output_data = '- 准备打卡😝...'
+        # 伪装地址
+        driver.command_executor._commands['set_permission'] = (
+            'POST', '/session/$sessionId/permissions')
+        print("=====================driver.command_executor._commands is successful=====================")
+        driver.execute(
+            'set_permission',
+            {
+                'descriptor': { 'name': 'geolocation' },
+                'state': 'granted'
+            }
+        )
+        print("=====================driver.execute is successful=====================")
+        
+        # 这块太坑人了, execute_cdp_cmd()这个方法不接受str值,需要将str转为float........
+        driver.execute_cdp_cmd(
+            'Emulation.setGeolocationOverride', {
+            'latitude': position['latitude'],
+            'longitude': position['longitude'],
+            'accuracy': position['accuracy']
+        })
+        
+        print("=====================driver.execute_cdp_cmd is successful=====================")
+        time.sleep(2)
+        #点击获取地理位置
+        area = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="xxdz41"]'))
+        )
+        area.click()
+        time.sleep(3)
+        pos = driver.find_element_by_xpath('//*[@id="app"]/div[2]/form/div[3]/div[2]/div/span/div[2]').text
+        output_data += f'- 当前定位地址:{pos}{address}\n'
+        #自己输入的地理位置
+        driver.find_element_by_xpath('//*[@id="app"]/div[2]/form/div[3]/div[2]/div/span/textarea').send_keys(address)
+
+        
+        # 提交：
+        commit =  WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[2]/form/div[18]/div/div/span/button'))
+        )
+        commit.click()
+        time.sleep(2)
+        output_data += "- 提交成功😝"
+        # 打卡结果信息
+        name = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[1]').text
+        gh = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[2]').text
+        date = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[3]').text
+        
+        output_data += '- 打卡信息:'
+        output_data += f'''
+                        >{{
+
+                        >     {name},
+                        >     {gh},
+                        >     {date}
+
+                        > }}
+                                '''
+        driver.get("https://sctapi.ftqq.com/" + wxkey +".send?title="+ username + "打卡成功😝" + "&desp=" + output_data)
+        print('打卡成功')
+    except Exception as e:
+        print("打卡失败")
+        output_data += e
+        print(e)
+        try:
+            status = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div').text
+            if status == '上级部门已确认':
+                output_data = '未到打卡时间🙃' 
+        except Exception as es:
+            print(e)
+            output_data += e
+        print(output_data)
+        driver.get("https://sctapi.ftqq.com/" + wxkey +".send?title="+ username + "打卡失败🙃,请自行打卡" + "&desp=" + output_data)
+         
+    driver.quit()
+def run():
+    env_dist = os.environ
+    position = dict({
+            "latitude": float(env_dist['latitude']),    # 34.226692,
+            "longitude": float(env_dist['longitude']),  # 
+            "accuracy": 100
+            })
+    task(env_dist['username'], env_dist['password'], env_dist['address'], position, env_dist['wxkey'])
+if __name__ == "__main__":
+    run()
+    
+    
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from lib2to3.pgen2 import driver
 import time
-import os
 import json
 import random
-# from email.mime.image import MIMEImage
+from email.mime.image import MIMEImage
 from smtplib import SMTP_SSL
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from apscheduler.schedulers.background import BackgroundScheduler
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
+
+
+# 微信server酱机器人推送(预留)
+def wxpost(driver, content):
+    with open("config.json", encoding='utf-8') as f:
+        config = json.load(f)
+    driver.get("https://sc.ftqq.com/"+config['wxkey']+".send?text="+content)
+    print("微信打卡成功")
 
 # 邮件推送
 def sendMail(mailInfo, userInfo, intitle, flag, address):
@@ -71,128 +78,109 @@ def sendMail(mailInfo, userInfo, intitle, flag, address):
     except Exception as e:
         print(e.with_traceback)
     
-def task(username, password, address, position, wxkey):
-    chrome_option = Options()
-
-    chrome_option.add_argument('--headless')
-    chrome_option.add_argument('--no-sandbox')
-    chrome_option.add_argument('window-size=1920x1080') # 指定浏览器分辨率
-    chrome_option.add_argument('--disable-gpu')
-    chrome_option.add_experimental_option('excludeSwitches', ['enable-automation'])
-    # action端
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_option)
-    # driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_option)
-    # win端
-    # driver = webdriver.Chrome(options=chrome_option)
-    # driver = webdriver.Chrome()
-    # driver.set_window_size(500, 940)
+def task(username, password, address):
+    # chrome_option = webdriver.ChromeOptions()
+    # 不知道为什么,这几天设置无头模式会获取不到定位,前两天都可以
+    # chrome_option.add_argument('--headless')
+    # chrome_option.add_argument('--no-sandbox')
+    # chrome_option.add_argument('--hide-scrollbars')
+    # chrome_option.add_experimental_option('excludeSwitches', ['enable-automation'])
+    # driver = webdriver.Chrome(chrome_options=chrome_option)
+    driver = webdriver.Chrome()
+    driver.set_window_size(500, 940)
     #登录
-    output_data = ""
-    url_login='https://cdjk.chd.edu.cn'
-    driver.get(url_login)
-    time.sleep(2)
-    driver.find_element_by_xpath('//*[@id="username"]').send_keys(username)
-    time.sleep(1)
-    driver.find_element_by_xpath('//*[@id="password"]').send_keys(password,Keys.ENTER)
-
-    # 判断是否在打卡时间段
     try:
-        output_data = '准备打卡😝...'
-        # 伪装地址
-        driver.command_executor._commands['set_permission'] = (
-            'POST', '/session/$sessionId/permissions')
-        print("=====================driver.command_executor._commands is successful=====================")
-        driver.execute(
-            'set_permission',
-            {
-                'descriptor': { 'name': 'geolocation' },
-                'state': 'granted'
-            }
-        )
-        print("=====================driver.execute is successful=====================")
-        driver.execute_cdp_cmd(
-            'Emulation.setGeolocationOverride', {
-            'latitude': position['latitude'],
-            'longitude': position['longitude'],
-            'accuracy': position['accuracy']
-        })
-        
-        print("=====================driver.execute_cdp_cmd is successful=====================")
-        time.sleep(2)
-        #点击获取地理位置
-        area = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="xxdz41"]'))
-        )
-        area.click()
-        time.sleep(3)
-        pos = driver.find_element_by_xpath('//*[@id="app"]/div[2]/form/div[3]/div[2]/div/span/div[2]').text
-        output_data += f'当前地址:{pos}{address}\n'
-        #自己输入的地理位置
-        driver.find_element_by_xpath('//*[@id="app"]/div[2]/form/div[3]/div[2]/div/span/textarea').send_keys(address)
-
-        
-        # 提交：
-        commit =  WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[2]/form/div[18]/div/div/span/button'))
-        )
-        commit.click()
-        time.sleep(2)
-        # 打卡结果信息
-        name = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[1]').text
-        gh = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[2]').text
-        date = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[3]').text
-        output_data += f':{name}\n{gh}\n{date}'
-        # 截图
-        # driver.save_screenshot(str(username) + "_success.png")
-        driver.get("https://sctapi.ftqq.com/" + wxkey +".send?title="+ username + "打卡成功😝" + "&desp=" + output_data)
-        print('打卡成功')
-    except Exception as e:
-        output_data += e
-        print(e)
-        try:
-            status = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div').text
-            if status == '上级部门已确认':
-                output_data = '未到打卡时间🙃' 
-        except Exception as es:
-            print(e)
-            output_data += e
-        print(output_data)
-        driver.get("https://sctapi.ftqq.com/" + wxkey +".send?title="+ username + "打卡失败🙃,请自行打卡" + "&desp=" + output_data)
-         
-    driver.quit()
-            # return True
-            # 截图
-            # driver.save_screenshot(str(username) + "_fail.png")
-            # return False
-        # 截图
-        # driver.save_screenshot(str(username) + "_fail.png")
-        # return False
-def run():
-    env_dist = os.environ
-    position = dict({
-            "latitude": 34.226692, # env_dist['latitude'],    # 34.226692,
-            "longitude": 108.954232, # env_dist['longitude'],  # 
+        url_login='https://ids.chd.edu.cn/authserver/login?service=http%3A%2F%2Fcdjk.chd.edu.cn%2FhealthPunch%2Findex%2Flogin'
+        driver.get(url_login)
+        time.sleep(4)
+        # 判断是否正确进入登陆页面
+        while True:
+            if driver.title == "统一身份认证平台":
+                print(driver.title)
+                break
+            driver.get(url_login)
+        # 获取用户与密码输入框并输入
+        driver.find_element_by_xpath('//*[@id="username"]').send_keys(username)
+        time.sleep(1)
+        driver.find_element_by_xpath('//*[@id="password"]').send_keys(password,Keys.ENTER)
+        # 如果跳转到打卡页面,退出循环
+        title = driver.title
+        currentPageUrl = driver.current_url
+        if title=='每日健康打卡' or "https://cdjk.chd.edu.cn" in currentPageUrl:
+            # 获取地理位置并提交
+            driver.execute_cdp_cmd(
+                "Browser.grantPermissions",  # 授权地理位置信息
+                {
+                    "origin": "https://www.chd.edu.cn/",
+                    "permissions": ["geolocation"]
+                },
+            )
+            # 伪装地址
+            Map_coordinates = dict({
+            "latitude": 34.226692,
+            "longitude": 108.954232,
             "accuracy": 100
             })
-    task(env_dist['username'], env_dist['password'], env_dist['address'], position, env_dist['wxkey'])
-    # sendMail(env_dist['email'], env_dist['username'], '自动打卡回执', add)
-    # position = dict({
-    #         "latitude":  34.226692,
-    #         "longitude": 108.954232,
-    #         "accuracy": 100
-    #         })
-    # with open('config.json', 'r', encoding='utf-8') as f:
-    #     CONFIG = json.load(f)
-    #     users = CONFIG['userInfo']
-    #     address = CONFIG['address']
-    #     mail = CONFIG['mailInfo']
 
-    # for user in users:
-    #     add = address[random.randint(1, len(address) - 1)]
-    #     flag = task(username = user['id'], password = user['pw'], address = add, wxkey='', position=position)
-    #     intitle = "自动打卡回执"
-        # sendMail(mailInfo=mail, userInfo=user, intitle=intitle, flag=flag, address=add)
-        # time.sleep(random.randint(120, 180))
+            driver.execute_cdp_cmd(
+                'Emulation.setGeolocationOverride', {
+                "latitude": Map_coordinates['latitude'],
+                "longitude": Map_coordinates['longitude'],
+                "accuracy": Map_coordinates['accuracy']
+            })
+            time.sleep(2)
+            #点击获取地理位置
+            area = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="xxdz41"]'))
+            )
+            area.click()
+            time.sleep(3)
+            #自己输入的地理位置
+            driver.find_element_by_xpath('//*[@id="app"]/div[2]/form/div[3]/div[2]/div/span/textarea').send_keys(address)
+
+            
+            # 提交：
+            commit =  WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[2]/form/div[18]/div/div/span/button'))
+            )
+            commit.click()
+            time.sleep(4)
+            # 截图
+            driver.save_screenshot(str(username) + "_success.png")
+            # print('OK!')
+            driver.quit()
+            return True
+        else:
+            # 截图
+            driver.save_screenshot(str(username) + "_fail.png")
+            return False
+    except Exception  as e:
+        # 截图
+        driver.save_screenshot(str(username) + "_fail.png")
+        return False
+def run():
+    with open('config.json', 'r', encoding='utf-8') as f:
+        CONFIG = json.load(f)
+        users = CONFIG['userInfo']
+        address = CONFIG['address']
+        mail = CONFIG['mailInfo']
+
+    for user in users:
+        add = address[random.randint(1, len(address) - 1)]
+        flag = task(username = user['id'], password = user['pw'], address = add)
+        intitle = "自动打卡回执"
+        sendMail(mailInfo=mail, userInfo=user, intitle=intitle, flag=flag, address=add)
+        time.sleep(random.randint(120, 180))
 if __name__ == "__main__":
     run()
+    # 定时任务,直接扔到实验室不用的电脑上😎
+    # scheduler = BackgroundScheduler(timezone='Asia/Shanghai')
+    # scheduler.add_job(run, 'cron', hour=7, minute=random.randint(10, 59))
+    # scheduler.add_job(run, 'cron', hour=13, minute=random.randint(10, 59))
+    # scheduler.start()
+    # try:
+    #     while True:
+    #         time.sleep(2)
+    # except Exception as e:
+    #     scheduler.shutdown()
     
